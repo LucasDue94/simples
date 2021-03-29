54faff91dba0b43efc8b4b8aa833cfda2b2a3731
Lucas Dué <lucasdue94@gmail.com>
2021-03-28 23:57:27 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simples';
+  private user = {
+    username: 'Simples Dental',
+    password: 'dental'
+  };
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Large)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    /*localStorage.setItem('app_simples_dental', JSON.stringify(this.user));
+    const appStorage = localStorage.getItem('app_simples_dental');*/
+  }
+
 }
